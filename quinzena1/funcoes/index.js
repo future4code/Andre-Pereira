908f2944bf9b0d9ab5804d7bcf8a8141dde3c79d
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-05-31 22:09:37 -0300

    Iniciado a atividade de interpretação.

---
+++ quinzena1/funcoes/index.js
@@ -0,0 +1,18 @@
+/*Exercicio de Interpretação de codigo
+
+ 1. 
+function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10)) 
+
+A.  10
+    50
+
+B.  Só imprimiria a ultima. 50  
+
+
+2.  
+*/
