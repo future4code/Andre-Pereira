00a21b85b89c1ddf1ebad37e0d9bbd9c9e7aeda8
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-06-03 13:56:31 -0300

    Finalizado primeira parte da atividade objetos.

---
+++ quinzena1/objetos/index.js
@@ -0,0 +1,24 @@
+// Atividade de interpretação de código
+//1A.
+/* Matheus Nachtergaele
+Virginia Cavendish
+canal: "Globo", horario: "14h" */
+
+//2A.
+    /* nome: "Juca", 
+	idade: 3, 
+	raca: "SRD"
+
+    nome: "Juba", 
+	idade: 3, 
+	raca: "SRD"
+
+    nome: "Jubo", 
+	idade: 3, 
+	raca: "SRD" */
+
+//2B.Spread realiza uma copia do objeto.
+
+//3A. False
+//3B. Ele retornou o valor da propriedade que estava sendo passada na função, porem a altura não existia essa propriedade, então nao retornaria nada.
+//--------------------------------------------------------------------------------------------------------------------------
