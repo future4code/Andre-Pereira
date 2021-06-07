f4aee18369338b770e6f161b8cc23379dfb448b2
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-06-06 21:06:07 -0300

    Concluído primeira parte do projeto BlackJack.

---
+++ quinzena2/blackjack/nossoJogo.js
@@ -0,0 +1,37 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+    console.log("Bem-vindo ao jogo de blackjack")
+    
+    if (confirm("Quer iniciar uma nova jogada?")) {
+        let userOne = comprarCarta()
+        let userTwo = comprarCarta()
+        let computerOne = comprarCarta()
+        let computerTwo = comprarCarta()
+    
+        let pontosUser = userOne.valor + userTwo.valor
+        let pontosComputer = computerOne.valor + computerTwo.valor
+    
+        console.log(`Usuário - cartas: ${userOne.texto} ${userTwo.texto} - ${pontosUser}`)
+        console.log(`Computador - cartas: ${computerOne.texto} ${computerTwo.texto} - ${pontosComputer}`)
+    
+        if (pontosUser > pontosComputer) {
+            console.log("O usuário ganhou!")
+        } else if (pontosComputer > pontosUser) {
+            console.log("O computador ganhou!")
+        } else if (pontosUser === pontosComputer) {
+            console.log("Empate!")
+        }
+    
+    } else {
+        console.log("O jogo acabou")
+    }
