e8931e12630351b1c4c342378a87204fd5189c76
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-06-07 22:40:24 -0300

    Iniciado atividade Condicionais.

---
+++ quinzena2/condicionais/index.js
@@ -0,0 +1,14 @@
+// Atividade de interpretação de código
+/* 
+1A. Ele verifica se é par e devolve a mensagem PASSOU NO TESTE, se ele não for um número par ele devolve NÃO PASSOU NO TESTE.
+1B. Passou no teste apenas os números que não resta nada pelo número dois, ou seja, números pares.
+1C. Não passou no teste são números que restão aluma coisa com o número dois, no entanto são números impares.
+
+2A. Ele serve para entregar o valor da fruta escolhida pelo usuario.
+2B. "O preço da fruta maçã é R$ 2,25"
+2C. Ela irá continuar lendo e entregara o valor do default, ou seja 5.
+
+3A. Ele tem uma condição informando que se o número escolhido pelo usuario for maio que zero, imprimir na tela passou no teste.
+3B. Se for 10 positivo ele vai passar no teste e imprimira a mensagem do console.log(), se for negativo não ira fazer nada.
+3C. Era dar um erro de variavel mensagem nao estara definida, porque ele nao entrara no if para guardar essa mensagem.
+*/
