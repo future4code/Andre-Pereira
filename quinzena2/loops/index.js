576b43b36e3398d22564c364769f9f4c798da417
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-06-10 19:13:39 -0300

    Concluido atividade Loops.

--- quinzena2/loops/index.js
+++ quinzena2/loops/index.js
@@ -35,20 +35,75 @@
 //_______________________________________________________________________________________________________________________________________________________
 // Atividade de escrita de código
 //1.
-let quantidadeBichos = Number(prompt("Quantos animais de estimação você tem?"))
-let totalBichos = 0
-let nomeBicho = []
+// let quantidadeBichos = Number(prompt("Quantos animais de estimação você tem?"))
+// let totalBichos = 0
+// let nomeBicho = []
 
-if (quantidadeBichos === 0) {
-    console.log("Que pena! Você pode adotar um pet!")
+// if (quantidadeBichos === 0) {
+//     console.log("Que pena! Você pode adotar um pet!")
 
-} else {
-    while (totalBichos < quantidadeBichos) {
-        nomeBicho.push(prompt("Digite um nome para o Pet"))
-        totalBichos++
-    }
-    console.log(nomeBicho.toString())
-}
+// } else {
+//     while (totalBichos < quantidadeBichos) {
+//         nomeBicho.push(prompt("Digite um nome para o Pet"))
+//         totalBichos++
+//     }
+//     console.log(nomeBicho.toString())
+// }
 
 //2.
+let arrayOriginal = [6, 8, 7, 53, 47, 120, 90, 180, 64]
+let menorNumero = 0
+let maiorNumero = 0
 
+function imprimirUmPorVez(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i])
+    }
+    return array
+}
+
+function dividirPorDez(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i] / 10)
+    }
+    return array
+}
+
+function encontrarPares(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            console.log(array[i])
+        }
+    }
+    return array
+}
+
+function encontrarIndice(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(`O elemento do índex ${i} é ${array[i]}`)
+    }
+    return array
+}
+
+for (let novoArray of arrayOriginal) {
+    if (novoArray > maiorNumero) {
+        maiorNumero = novoArray
+    }
+}
+
+for (let i = 0; i < arrayOriginal.length; arrayOriginal++) {
+    if (i < arrayOriginal[i]) {
+        menorNumero = arrayOriginal[i]
+    }
+}
+
+
+imprimirUmPorVez(arrayOriginal)
+
+dividirPorDez(arrayOriginal)
+
+encontrarPares(arrayOriginal)
+
+encontrarIndice(arrayOriginal)
+
+console.log(`O maior número é ${maiorNumero} e o menor é ${menorNumero}.`)
