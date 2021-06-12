649a15e2495c7eced18d9da1e8a3431f8ec6fce9
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-06-12 18:25:56 -0300

    Iniciado implementação do desafio.

--- quinzena2/blackjack/desafio.js
+++ quinzena2/blackjack/desafio.js
@@ -13,17 +13,38 @@
 
 console.log("Bem-vindo ao jogo de blackjack")
 let vamosJogar = confirm("Vamos iniciar o jogo BlackJack?")
+if (vamosJogar === false) {
+   console.log("Você Desistiu, aperte F5 para recomeçar.")
+}
 
 while (vamosJogar) {
+
    if (vamosJogar) {
       let userOne = comprarCarta()
       let userTwo = comprarCarta()
       let computerOne = comprarCarta()
       let computerTwo = comprarCarta()
+      let cartaNovamente = ((userOne.valor === 11 && userOne.texto === 'A') && (userTwo.valor === 11 && userTwo.texto === 'A')) || ((computerOne.valor === 11 && computerOne.texto === 'A') && (computerTwo.valor === 11 && computerTwo.texto === 'A'))
 
+
+      while (cartaNovamente) {
+         userOne = comprarCarta()
+         userTwo = comprarCarta()
+         computerOne = comprarCarta()
+         computerTwo = comprarCarta()
+         cartaNovamente = confirm("Temos que comprar as cartas novamente.")
+      }
 
       let pontosUser = userOne.valor + userTwo.valor
       let pontosComputer = computerOne.valor + computerTwo.valor
+
+      let comprarNovaCarta = confirm(`Sua primeira carta é ${userOne.texto} e ${userTwo.texto}. A carta revelada do computador é ${computerOne.texto}
+      Deseja comprar mais uma carta?`)
+
+      if (comprarNovaCarta) {
+         let novaCarta = comprarCarta()
+         pontosUser += novaCarta.valor
+      }
 
       console.log(`Usuário - cartas: ${userOne.texto} ${userTwo.texto} - ${pontosUser}`)
       console.log(`Computador - cartas: ${computerOne.texto} ${computerTwo.texto} - ${pontosComputer}`)
