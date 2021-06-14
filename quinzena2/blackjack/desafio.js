e84dcbcc433554edba2099323825d129321417d0
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-06-13 22:00:59 -0300

    Refatorado o desafio blackjack.

--- quinzena2/blackjack/desafio.js
+++ quinzena2/blackjack/desafio.js
@@ -28,39 +28,34 @@ while (vamosJogar) {
       let computerOne = comprarCarta()
       let computerTwo = comprarCarta()
       cartasComputador.push(computerOne.texto, computerTwo.texto)
-      let cartaNovamente = ((userOne.valor === 11 && userOne.texto === 'A') && (userTwo.valor === 11 && userTwo.texto === 'A')) || ((computerOne.valor === 11 && computerOne.texto === 'A') && (computerTwo.valor === 11 && computerTwo.texto === 'A'))
 
       // Validação dos Ases
-      while (cartaNovamente) {
+      while (((userOne.valor === 11 && userOne.texto === 'A') && (userTwo.valor === 11 && userTwo.texto === 'A')) || ((computerOne.valor === 11 && computerOne.texto === 'A') && (computerTwo.valor === 11 && computerTwo.texto === 'A'))) {
          userOne = comprarCarta()
          userTwo = comprarCarta()
          computerOne = comprarCarta()
          computerTwo = comprarCarta()
-         cartaNovamente = confirm("Temos que comprar as cartas novamente.")
       }
 
       // A pontuação geral
       let pontosUser = userOne.valor + userTwo.valor
       let pontosComputer = computerOne.valor + computerTwo.valor
 
-      let comprarNovaCarta = confirm(`Sua primeira carta é ${userOne.texto} e ${userTwo.texto}. A carta revelada do computador é ${computerOne.texto}
-      Deseja comprar mais uma carta?`)
 
       // Compra da nova carta para o jogador
-      while (comprarNovaCarta) {
-         let novaCartaJogador = comprarCarta()
-         cartasJogador.push(novaCartaJogador.texto)
-         pontosUser += novaCartaJogador.valor
-         comprarNovaCarta = confirm(`Sua primeira carta é ${cartasJogador}. A carta revelada do computador é ${computerOne.texto}
-      Deseja comprar mais uma carta?`)
-         if (comprarNovaCarta === false) {
+      while (pontosUser < 21) {
+         if (comprarNovaCarta = confirm(`Suas cartas são ${cartasJogador}. A carta revelada do computador é ${computerOne.texto}
+Deseja comprar mais uma carta?`)) {
+            let novaCartaJogador = comprarCarta()
+            cartasJogador.push(novaCartaJogador.texto)
+            pontosUser += novaCartaJogador.valor
+         } else {
             break
          }
       }
 
-
       // Compra das novas cartas do computador
-      while (pontosComputer <= pontosUser) {
+      while (pontosUser <= 21 && pontosComputer < pontosUser) {
          let novaCartaComputador = comprarCarta()
          cartasComputador.push(novaCartaComputador.texto)
          pontosComputer += novaCartaComputador.valor
@@ -82,7 +77,7 @@ while (vamosJogar) {
       }
 
       alert("Suas cartas são " + cartasJogador + ". Sua pontuação é " + pontosUser + ".\n" +
-         "As cartas do computador são " + cartasComputador + "." + "A pontuação do computador é " + pontosComputer + ".\n" +
+         "As cartas do computador são " + cartasComputador + "." + " A pontuação do computador é " + pontosComputer + ".\n" +
          mensagem)
 
    } else {
