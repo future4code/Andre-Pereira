5f7cc645c036cc588c307c2f7f2b704fbc8d25e8
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-09-18 18:16:47 -0300

    Implementado cadastro de paises e formularo

--- quinzena9/labex/src/pages/ApplicationFormPage.jsx
+++ quinzena9/labex/src/pages/ApplicationFormPage.jsx
@@ -1,23 +1,131 @@
-import React from 'react'
-import { useHistory } from 'react-router';
+import React, { useState, useEffect } from "react";
+import { useForm } from "../hooks/useForm";
+import { useHistory } from "react-router";
+import { countries } from "../constants/countries";
+import axios from "axios";
 
 export default function ApplicationFormPage() {
-const history = useHistory()
+  const history = useHistory();
+  const [viagens, setViagens] = useState([]);
 
-const irInicio = () => {
-    history.goBack("/")
-}
+  useEffect(() => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labeX/darvas/trips"
+      )
+      .then((res) => {
+        setViagens(res.data.trips);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }, []);
 
-const irListaViagens = () => {
-    history.push("/list-trips")
-}
+  const { formulario, alterarDadosFormulario } = useForm({
+    name: "",
+    age: "",
+    applicationText: "",
+    profession: "",
+    country: "",
+  });
 
-    return (
-        <div>
-            <p>Formulário do Candidato</p>
-            <button onClick={irInicio} >Ínicio</button>
-            <button onClick={irListaViagens} >Lista de Viagens</button>
+  const irInicio = () => {
+    history.goBack("/");
+  };
 
-        </div>
-    )
+ 
+
+  const enviarFormularios = (id) => {
+    id.preventDefault();
+
+    const body = {
+      name: formulario.name,
+      age: formulario.age,
+      applicationText: formulario.applicationText,
+      profession: formulario.profession,
+      country: formulario.country,
+    };
+
+    axios
+      .post(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labeX/darvas/trips/${id}/apply`,
+        body
+      )
+      .then((response) => {
+        console.log(formulario.country);
+        alert("Dados enviado com sucesso");
+        alterarDadosFormulario({
+          name: "",
+          age: "",
+          applicationText: "",
+          profession: "",
+          country: "",
+        });
+      });
+  };
+
+  return (
+    <>
+      <p>Preencha os dados para Iniciar essa viagem</p>
+      <form onSubmit={enviarFormularios}>
+        <select>
+          <option>Selecione sua viagem</option>
+          {viagens.map((cadaViagem) => {
+            return <option>{cadaViagem.name}</option>;
+          })}
+        </select>
+        <input
+          required
+          placeholder={"Nome completo"}
+          type={"text"}
+          name={"name"}
+          onChange={alterarDadosFormulario}
+          value={formulario.name}
+        />
+        <input
+          required
+          placeholder={"Idade"}
+          type={"number"}
+          name={"age"}
+          onChange={alterarDadosFormulario}
+          value={formulario.age}
+        />
+        <textarea
+          placeholder={
+            "Observação para essa viagem. (200 caraceteres no máximo.)"
+          }
+          type={"text"}
+          name={"applicationText"}
+          onChange={alterarDadosFormulario}
+          value={formulario.applicationText}
+          maxLength={200}
+        />
+        <input
+          required
+          placeholder={"Profissão"}
+          type={"text"}
+          name={"profession"}
+          onChange={alterarDadosFormulario}
+          value={formulario.profession}
+        />
+        <select>
+          <option>Selecione o seu país</option>
+          {countries.map((cadaPais) => {
+            return (
+              <option
+                name={"country"}
+                value={formulario.country}
+                onChange={alterarDadosFormulario}
+                required
+              >
+                {cadaPais}
+              </option>
+            );
+          })}
+        </select>
+        <button>Salvar</button>
+      </form>
+      <button onClick={irInicio}>Ínicio</button>
+    </>
+  );
 }
--- quinzena9/labex/src/pages/ListTripsPage.jsx
+++ quinzena9/labex/src/pages/ListTripsPage.jsx
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router";
+import axios from "axios";
 
 export default function ListTripsPage() {
   const history = useHistory();
+  const [listaViagens, setListaViagens] = useState([]);
 
   const irInicio = () => {
     history.push("/");
   };
 
+  const irFormulario = () => {
+    history.push("/form");
+  };
+
+  const listarViagens = async () => {
+    try {
+      const response = await axios.get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labeX/darvas/trips"
+      );
+      setListaViagens(response.data.trips);
+    } catch {
+      alert("Deu erro");
+    }
+  };
+
+  useEffect(() => {
+    listarViagens();
+  }, []);
+
+  const viagens = listaViagens.map((cadaViagem) => {
+    return (
+      <div>
+        <h2>{cadaViagem.name}</h2>
+        <p> {cadaViagem.description} </p>
+        <p> {cadaViagem.planet} </p>
+        <p> Duração em dias: {cadaViagem.durationInDays} </p>
+        <p> {cadaViagem.description} </p>
+        <p> {cadaViagem.date} </p>
+      </div>
+    );
+  });
+
   return (
     <div>
       <p>Lista de Viagens</p>
       <button onClick={irInicio}>Ínicio</button>
+      <button onClick={irFormulario}>Inscrição para Viagem</button>
+      {viagens}
     </div>
   );
 }
